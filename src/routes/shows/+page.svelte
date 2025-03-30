5b6abf1ad53a4d7f3d9f7e02149ee90fec9ffb8f
janiceb75 <janicebailey75@gmail.com>
2025-03-30 09:31:15 -0400

    commit

--- src/routes/shows/+page.svelte
+++ src/routes/shows/+page.svelte
@@ -2,4 +2,4 @@
 	<div class="card max-w-full items-center justify-center">Default</div>
 	<div class="divider divider-vertical mx-0 h-44"></div>
 	<div class="card max-w-full items-center justify-center">Default</div>
-</div>
+</div> 
